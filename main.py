bd8809b792352487e33b5639f011fa08d9e23f8d
Martin Reynolds <62625037+martinbreynolds@users.noreply.github.com>
2022-10-09 20:06:31 +0000

    Random Choice Setup

--- main.py
+++ main.py
@@ -26,4 +26,21 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
+
+hands = [rock, paper, scissors]
+user_choice = int(input("What do you chose?  Type 0 for Rock, 1 for Paper, and 2 for Scissors"))
+
+print(hands[user_choice])
+
+print("Computer chose:")
+
+computer_choice = random.randint(0,len(hands)-1)
+print(hands[computer_choice])
+
+#Winners
+
+#Rock wins against scissors.
+#Scissors win against paper.
+#Paper wins against rock.
 
